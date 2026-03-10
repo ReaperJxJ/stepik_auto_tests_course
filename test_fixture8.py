6d68e8d3472f182011148fa21653e1650a6f2b7d
zyryanov <roman13270@gmail.com>
2026-03-10 21:47:04 +0500

    Добавил Скип теста

--- test_fixture8.py
+++ test_fixture8.py
@@ -16,12 +16,13 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
+    @pytest.mark.skip
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
     @pytest.mark.regression
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
