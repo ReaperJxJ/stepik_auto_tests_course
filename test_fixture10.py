bd29fab8a4ade724367aa74902aa9e7992ca0844
zyryanov <roman13270@gmail.com>
2026-03-10 22:18:08 +0500

    Починил тест, получил xpass

--- test_fixture10.py
+++ test_fixture10.py
@@ -27,5 +27,5 @@ class TestMainPage1():
     @pytest.mark.xfail
     def test_guest_should_see_search_button_on_the_main_page(self, browser):
         browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, "button.favorite")
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
